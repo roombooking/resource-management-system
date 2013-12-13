b6671240f01e5f0086ec8d589d90ef98b346e6a0
faabsen <faabsen@gmail.com>
2013-12-13 15:35:56 +0000

    Test
    git-svn-id: https://dev.qu.tu-berlin.de/svn/pjroombooking/trunk@6 77ec3233-479d-4028-a864-0deb41a0d16d

---
+++ module/Album/Module.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/Album for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+		    // if we're in a namespace deeper than one level we need to fix the \ in the path
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        // You may not need to do this if you're doing it elsewhere in your
+        // application
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+    public function getServiceConfig()
+     {
+         return array(
+             'factories' => array(
+                 'Album\Model\AlbumTable' =>  function($sm) {
+                     $tableGateway = $sm->get('AlbumTableGateway');
+                     $table = new AlbumTable($tableGateway);
+                     return $table;
+                 },
+                 'AlbumTableGateway' => function ($sm) {
+                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                     $resultSetPrototype = new ResultSet();
+                     $resultSetPrototype->setArrayObjectPrototype(new Album());
+                     return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                 },
+             ),
+         );
+     }
+ }
---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/Album for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+     {
+     }
+
+     public function addAction()
+     {
+     }
+
+     public function editAction()
+     {
+     }
+
+     public function deleteAction()
+     {
+     }
+}
