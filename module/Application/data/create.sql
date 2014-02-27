82aecbe4a440ec1dd289221dda965f3c8c9f646f
mplatt <moritz.platt@googlemail.com>
2014-02-27 10:25:15 +0000

    initial sql scripts
    git-svn-id: https://dev.qu.tu-berlin.de/svn/pjroombooking/trunk@20 77ec3233-479d-4028-a864-0deb41a0d16d

--- module/Application/data/create.sql
+++ module/Application/data/create.sql
@@ -1,6 +1,6 @@
 /*
 Created: 06/12/2013
-Modified: 14/12/2013
+Modified: 27/02/2014
 Model: MySQL 5.1
 Database: MySQL 5.1
 */
@@ -11,214 +11,179 @@ Database: MySQL 5.1
 
 CREATE TABLE Users
 (
-  userid Varchar(767) NOT NULL
-  COMMENT 'The users LDAP userid / uid. Accepts uids up to 767 chars only.',
-  roleid Int,
-  role Int NOT NULL DEFAULT 0
-  COMMENT 'The role of the user according to the Roles table'
+  userid Int UNSIGNED NOT NULL AUTO_INCREMENT,
+  roleid Int UNSIGNED NOT NULL,
+  ldapid Int UNSIGNED NOT NULL,
+  loginname Varchar(256) NOT NULL,
+  firstname Varchar(256),
+  lastname Varchar(256),
+  emailaddress Varchar(256)
+  COMMENT 'The maximum length of a valid e-mail address is 254 characters.',
+ PRIMARY KEY (userid),
+ UNIQUE userid (userid)
 )
 ;
 
-ALTER TABLE Users ADD PRIMARY KEY (userid)
+ALTER TABLE Users ADD UNIQUE ldapid (ldapid)
 ;
 
 -- Table Roles
 
 CREATE TABLE Roles
 (
-  roleid Int NOT NULL,
-  rolename Varchar(767)
+  roleid Int UNSIGNED NOT NULL AUTO_INCREMENT,
+  rolename Varchar(256),
+ PRIMARY KEY (roleid)
 )
-;
-
-ALTER TABLE Roles ADD PRIMARY KEY (roleid)
 ;
 
 -- Table Powers
 
 CREATE TABLE Powers
 (
-  roleid Int NOT NULL,
-  show_calendar Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show the calendar',
-  show_appointment_details Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show appointment details',
-  export_appointment Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to export appointments as ICS file',
-  show_room_list Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show the room list',
-  show_room_details Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show room details',
-  show_equipment_list Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show the equipment list',
-  show_equipment_details Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show equipment details',
-  show_user_details Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to show user details',
-  edit_user_roles Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to edit user roles',
-  add_appointment Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to add appointments',
-  edit_own_appointment Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to edit her/his own appointments',
-  delete_own_appointment Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to delete her/his own appointments',
-  delete_foreign_appointment Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to delete other users appointments',
-  edit_foreign_appointment Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to edit other users appointments',
-  generate_appointment_suggestion Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to generate appointment suggestions',
-  add_prebooking Bool DEFAULT FALSE
-  COMMENT 'Whether the user is allowed to make pre-bookings',
-  edit_own_prebooking Bool DEFAULT FALSE,
-  delete_own_prebooking Bool DEFAULT FALSE,
-  delete_foreign_prebooking Bool DEFAULT FALSE,
-  edit_foreign_prebooking Bool DEFAULT FALSE,
-  edit_room_description Bool DEFAULT FALSE,
-  add_room Bool DEFAULT FALSE,
-  delete_room Bool DEFAULT FALSE,
-  add_equipment Bool DEFAULT FALSE,
-  edit_equipment_description Bool DEFAULT FALSE,
-  delete_equipment Bool DEFAULT FALSE
+  powerid Int UNSIGNED NOT NULL AUTO_INCREMENT,
+  roleid Int UNSIGNED NOT NULL,
+  module Varchar(256) NOT NULL,
+  controller Varchar(256) NOT NULL,
+  action Varchar(256) NOT NULL,
+ PRIMARY KEY (powerid)
 )
 ;
 
-ALTER TABLE Powers ADD PRIMARY KEY (roleid)
-;
+-- Table Bookings
 
--- Table Rooms
-
-CREATE TABLE Rooms
+CREATE TABLE Bookings
 (
-  roomid Int NOT NULL AUTO_INCREMENT,
-  containmentid Int NOT NULL,
-  description Varchar(120) NOT NULL,
- PRIMARY KEY (roomid)
+  bookingid Int UNSIGNED NOT NULL AUTO_INCREMENT,
+  booking_userid Int UNSIGNED NOT NULL,
+  responsible_userid Int UNSIGNED,
+  resourceid Int UNSIGNED NOT NULL,
+  name Varchar(256),
+  description Text,
+  participant_description Text,
+  start Datetime NOT NULL,
+  end Datetime NOT NULL,
+  isprebooking Bool NOT NULL DEFAULT 0,
+  isdeleted Bool NOT NULL DEFAULT 0,
+ PRIMARY KEY (bookingid),
+ UNIQUE bookingid (bookingid)
 )
 ;
 
--- Table Roomcontainments
+-- Table Resources
 
-CREATE TABLE Roomcontainments
+CREATE TABLE Resources
 (
-  containmentid Int NOT NULL,
-  parent Int,
-  title Varchar(120) NOT NULL,
-  description Text
+  resourceid Int UNSIGNED NOT NULL AUTO_INCREMENT,
+  isbookable Bool NOT NULL DEFAULT 0,
+  isdeleted Bool NOT NULL DEFAULT 0,
+  name Varchar(256),
+  description Text,
+ PRIMARY KEY (resourceid)
 )
 ;
 
-ALTER TABLE Roomcontainments ADD PRIMARY KEY (containmentid)
-;
+-- Table Containments
 
--- Table Roombookings
-
-CREATE TABLE Roombookings
+CREATE TABLE Containments
 (
-  bookingid Int NOT NULL AUTO_INCREMENT,
-  userid_booking Varchar(767) NOT NULL,
-  roomid_booking Int NOT NULL,
-  is_prebooking Bool DEFAULT FALSE,
-  userid_responsible Text,
-  booking_from Datetime NOT NULL,
-  booking_to Datetime NOT NULL,
- PRIMARY KEY (bookingid)
+  containmentid Int UNSIGNED NOT NULL AUTO_INCREMENT,
+  hirachyid Int UNSIGNED NOT NULL,
+  parent Int UNSIGNED,
+  child Int UNSIGNED,
+  name Varchar(256),
+  description Text,
+ PRIMARY KEY (containmentid)
 )
+;
+
+-- Table Hirachies
+
+CREATE TABLE Hirachies
+(
+  hirachyid Int UNSIGNED NOT NULL
+)
+;
+
+ALTER TABLE Hirachies ADD PRIMARY KEY (hirachyid)
+;
+
+ALTER TABLE Hirachies ADD UNIQUE hirachyid (hirachyid)
 ;
 
 -- Table Equipments
 
 CREATE TABLE Equipments
 (
-  equipmentid Int NOT NULL AUTO_INCREMENT,
-  roomid_equipment Int NOT NULL,
-  containmentid Int NOT NULL,
-  description Varchar(120),
- PRIMARY KEY (equipmentid)
+  equipmentid Int UNSIGNED NOT NULL,
+  resourceid Int UNSIGNED
 )
 ;
 
--- Table Equipmentcontainments
+ALTER TABLE Equipments ADD PRIMARY KEY (equipmentid)
+;
 
-CREATE TABLE Equipmentcontainments
+-- Table Places
+
+CREATE TABLE Places
 (
-  containmentid Int NOT NULL,
-  parent Int,
-  title Varchar(120) NOT NULL,
-  description Text
+  placeid Int UNSIGNED NOT NULL,
+  resourceid Int UNSIGNED,
+  size Float,
+  seatnumber Int
 )
 ;
 
-ALTER TABLE Equipmentcontainments ADD PRIMARY KEY (containmentid)
+ALTER TABLE Places ADD PRIMARY KEY (placeid)
 ;
 
--- Table Roomattributes
+-- Table Bookingrelations
 
-CREATE TABLE Roomattributes
+CREATE TABLE Bookingrelations
 (
-  roomid_attribute Int NOT NULL
+  masterbookingid Int UNSIGNED NOT NULL,
+  slavebookingid Int UNSIGNED NOT NULL
 )
 ;
 
-ALTER TABLE Roomattributes ADD PRIMARY KEY (roomid_attribute)
-;
-
--- Table Equipmentattributes
-
-CREATE TABLE Equipmentattributes
-(
-  equipmentid Int NOT NULL
-)
-;
-
-ALTER TABLE Equipmentattributes ADD PRIMARY KEY (equipmentid)
-;
-
--- Table Equipmentbookings
-
-CREATE TABLE Equipmentbookings
-(
-  bookingid Int NOT NULL,
-  equipmentid Int NOT NULL
-)
-;
-
-ALTER TABLE Equipmentbookings ADD PRIMARY KEY (bookingid,equipmentid)
+ALTER TABLE Bookingrelations ADD PRIMARY KEY (slavebookingid,masterbookingid)
 ;
 
 -- Create relationships section ------------------------------------------------- 
 
-ALTER TABLE Rooms ADD CONSTRAINT is_contained_in_1 FOREIGN KEY (containmentid) REFERENCES Roomcontainments (containmentid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Powers ADD CONSTRAINT has_1 FOREIGN KEY (roleid) REFERENCES Roles (roleid) ON DELETE CASCADE ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Powers ADD CONSTRAINT has_3 FOREIGN KEY (roleid) REFERENCES Roles (roleid) ON DELETE CASCADE ON UPDATE NO ACTION
+ALTER TABLE Users ADD CONSTRAINT has_2 FOREIGN KEY (roleid) REFERENCES Roles (roleid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Roombookings ADD CONSTRAINT books_room_1 FOREIGN KEY (userid_booking) REFERENCES Users (userid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Bookings ADD CONSTRAINT books_1 FOREIGN KEY (booking_userid) REFERENCES Users (userid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Roombookings ADD CONSTRAINT books_room_2 FOREIGN KEY (roomid_booking) REFERENCES Rooms (roomid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Bookings ADD CONSTRAINT contains_1 FOREIGN KEY (resourceid) REFERENCES Resources (resourceid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Equipments ADD CONSTRAINT is_contained_in_2 FOREIGN KEY (containmentid) REFERENCES Equipmentcontainments (containmentid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Containments ADD CONSTRAINT is_2 FOREIGN KEY (parent) REFERENCES Resources (resourceid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Roomattributes ADD CONSTRAINT has_1 FOREIGN KEY (roomid_attribute) REFERENCES Rooms (roomid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Containments ADD CONSTRAINT is_1 FOREIGN KEY (child) REFERENCES Resources (resourceid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Equipmentattributes ADD CONSTRAINT has_2 FOREIGN KEY (equipmentid) REFERENCES Equipments (equipmentid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Containments ADD CONSTRAINT contains_2 FOREIGN KEY (hirachyid) REFERENCES Hirachies (hirachyid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Users ADD CONSTRAINT has_4 FOREIGN KEY (roleid) REFERENCES Roles (roleid) ON DELETE RESTRICT ON UPDATE NO ACTION
+ALTER TABLE Bookings ADD CONSTRAINT books_2 FOREIGN KEY (responsible_userid) REFERENCES Users (userid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Equipments ADD CONSTRAINT contains_3 FOREIGN KEY (roomid_equipment) REFERENCES Rooms (roomid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Equipments ADD CONSTRAINT extends_1 FOREIGN KEY (resourceid) REFERENCES Resources (resourceid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Equipmentbookings ADD CONSTRAINT contains_1 FOREIGN KEY (bookingid) REFERENCES Roombookings (bookingid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Places ADD CONSTRAINT extends_2 FOREIGN KEY (resourceid) REFERENCES Resources (resourceid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
-ALTER TABLE Equipmentbookings ADD CONSTRAINT contains_2 FOREIGN KEY (equipmentid) REFERENCES Equipments (equipmentid) ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE Bookingrelations ADD CONSTRAINT is_3 FOREIGN KEY (masterbookingid) REFERENCES Bookings (bookingid) ON DELETE RESTRICT ON UPDATE NO ACTION
+;
+
+ALTER TABLE Bookingrelations ADD CONSTRAINT is_4 FOREIGN KEY (slavebookingid) REFERENCES Bookings (bookingid) ON DELETE RESTRICT ON UPDATE NO ACTION
 ;
 
--- module/Application/data/populate.sql
+++ module/Application/data/populate.sql
@@ -1,3 +1,11 @@
 /*
- *	TODO
+ *	Roles
  */
+ALTER TABLE roombooking.Roles AUTO_INCREMENT=1;
+INSERT INTO roombooking.Roles (rolename) VALUES ('Administrator'), ('Custodian'), ('User'), ('Guest');
+
+/*
+ *	Admin Powers
+ */
+INSERT INTO roombooking.Powers (powerid, roleid, module, controller, action) VALUES (1, 1, '%', '%', '%');
+ 
