0977cf41a45b4b77d44af33c8cadc68193b9731a
mplatt <moritz.platt@googlemail.com>
2014-02-27 20:21:01 +0000

    clean local config
    git-svn-id: https://dev.qu.tu-berlin.de/svn/pjroombooking/trunk@33 77ec3233-479d-4028-a864-0deb41a0d16d

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -3,54 +3,20 @@ return array(
         'dbParams' => array(
         		'database' => 'roombooking',
         		'hostname' => 'localhost',
-        		'username' => 'qu',
-        		'password' => '557PqYeQyN5EYMxR',
+        		'username' => 'root',
+        		'password' => '',                         // Dont' check in passwords
         ),
         'ldap' => array(
-//                 'server2' => array
-//                 (
-//                 		'host' => '192.168.178.25',
-//                 		'accountDomainName' => 'roombooking.qu.tu-berlin.de',
-//                 		'accountDomainNameShort' => 'roombooking.qu.tu-berlin.de',
-//                 		'accountCanonicalForm' => 3,
-//                 		'username' => 'CN=admin,DC=roombooking,DC=qu,DC=tu-berlin,DC=de',
-//                 		'password' => 'r00mb00king',
-//                 		'baseDn' => 'DC=roombooking,DC=qu,DC=tu-berlin,DC=de',
-//                 		'bindRequiresDn' => true,
-//                      'useStartTls' => true,                
-//                 ),
-//                 'server3' => array
-//                 (
-//                 		'host' => '192.168.178.77',
-//                 		'accountDomainName' => 'roombooking.qu.tu-berlin.de',
-//                 		'accountDomainNameShort' => 'roombooking.qu.tu-berlin.de',
-//                 		'accountCanonicalForm' => 3,
-//                 		'username' => 'CN=admin,DC=roombooking,DC=qu,DC=tu-berlin,DC=de',
-//                 		'password' => 'r00mb00king',
-//                 		'baseDn' => 'DC=roombooking,DC=qu,DC=tu-berlin,DC=de',
-//                 		'bindRequiresDn' => true,
-//                      'useStartTls' => true,              
-//                 ),
                 'server1' => array
                 (
-                		'host' => '192.168.23.31',
+                		'host' => '127.0.0.1',
                 		'accountDomainName' => 'roombooking.qu.tu-berlin.de',
                 		'accountDomainNameShort' => 'roombooking.qu.tu-berlin.de',
                 		'accountCanonicalForm' => 2,
                 		'username' => 'CN=admin,DC=roombooking,DC=qu,DC=tu-berlin,DC=de',
-                		'password' => 'r00mb00king',
+                		'password' => '',                 // Dont' check in passwords
                 		'baseDn' => 'DC=roombooking,DC=qu,DC=tu-berlin,DC=de',
-                		'bindRequiresDn' => true,
-                        //'useStartTls' => true,
-                ),
-//         		'server2' => array // AC
-//         		(
-//         				'host' => 'dc1.w.net',
-//         				'useStartTls' => 1,
-//         				'accountDomainName' => 'w.net',
-//         				'accountDomainNameShort' => 'W',
-//         				'accountCanonicalForm' => 3,
-//         				'baseDn' => 'CN=Users,DC=w,DC=net',
-//         		),
-        ),
+                		'bindRequiresDn' => true
+                )
+        )
 );
